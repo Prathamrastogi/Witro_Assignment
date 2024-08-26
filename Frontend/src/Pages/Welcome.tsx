4f94793670c38853d787527a7a0ec72d13c5804f
Pratham Rastogi <112170092+Prathamrastogi@users.noreply.github.com>
2024-08-26 23:19:23 +0530

    Update Welcome.tsx

--- Frontend/src/Pages/Welcome.tsx
+++ Frontend/src/Pages/Welcome.tsx
@@ -25,7 +25,7 @@ const Welcome: React.FC = () => {
 
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/auth/user",
+          "https://witro-assignment.onrender.com/api/auth/user",
           {
             headers: { Authorization: `Bearer ${token}` },
           }
