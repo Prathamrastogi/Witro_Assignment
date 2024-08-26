42495136da4da3ba81e4aa6e7a271162f21d4d15
Pratham Rastogi <112170092+Prathamrastogi@users.noreply.github.com>
2024-08-26 23:18:34 +0530

    Update Register.tsx

--- Frontend/src/Pages/Register.tsx
+++ Frontend/src/Pages/Register.tsx
@@ -67,7 +67,7 @@ const Register: React.FC = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://witro-assignment.onrender.com/api/auth/register",
         {
           firstName,
           lastName,
